3c2c6c633a5a1f15fd3556fb3639e59dca01bbda
Yasin Tezcan <yasintezcan@yahoo.com>
2023-10-15 17:31:58 +0300

    feat: proje

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    {
+      name: "buzdolabı",
+    },
+    {
+      name: "test",
+    },
+    {
+      name: "araba",
+    },
+    {
+      name: "deneme",
+    },
+  ];
 
+  const [search, setSearch] = useState("");
+
+  const handleOnChange = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {search}
+      <input type="text" name="" onChange={handleOnChange} />
+      <input type="file" name="" onChange={handleOnChange} />
+      {products
+        .filter((item) => {
+          return item.name.includes(search);
+        })
+        .map((item, index) => (
+          <div key={index}>{item.name}</div>
+        ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
